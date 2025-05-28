c8762542b304713dc29dbd7a943afbfbae3043f5
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-27 18:13:36 -0700

    fmt

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -1,3 +1,5 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
+
 resource "google_compute_subnetwork" "hqinternal" {
   name                     = "hqinternal"
   ip_cidr_range            = "10.100.10.0/24"
@@ -12,6 +14,7 @@ resource "google_compute_subnetwork" "regional_proxy_subnet" {
   name          = "regional-proxy-subnet"
   region        = "us-central1"
   ip_cidr_range = "10.0.0.0/24"
+  # This purpose reserves this subnet for regional Envoy-based load balancers
   purpose       = "REGIONAL_MANAGED_PROXY"
   network       = google_compute_network.main.id
   role          = "ACTIVE"
