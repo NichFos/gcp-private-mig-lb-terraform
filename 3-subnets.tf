11933e38804e56fd70986bbad3bba6c94d57efa2
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-27 19:13:09 -0700

    keep proxy subnet cidr correct

--- 3-subnets.tf
+++ 3-subnets.tf
@@ -13,7 +13,7 @@ resource "google_compute_subnetwork" "hqinternal" {
 resource "google_compute_subnetwork" "regional_proxy_subnet" {
   name          = "regional-proxy-subnet"
   region        = "us-central1"
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = "10.100.0.0/24"
   # This purpose reserves this subnet for regional Envoy-based load balancers
   purpose       = "REGIONAL_MANAGED_PROXY"
   network       = google_compute_network.main.id
